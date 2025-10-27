ec2656a4020d2f2a45367095aa0b607255394623
Ludvig <ludvi@Ludvigsdata.localdomain>
2025-10-27 16:02:52 +0100

    Duplicate1.c Done

---
+++ C/Duplicate.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool containsDuplicate(int* nums, const int numsSize) {
+    
+    int list[numsSize]; 
+
+    int i; 
+    int x; 
+    for(int i = 0; i < numsSize; ++i){
+        x = nums[i]; 
+        if(1 != list[x]){
+            list[x] = 1;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
+}
+
+int main () { 
+    int nums[] = {1,2,3,4};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    printf("%d", containsDuplicate(nums, numsSize));
+    printf("\n");
+    return 0;
+}
