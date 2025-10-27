7587d0830b668e3c72d2a7100a0ed2601b0decfb
Ludvig <ludvig.esbjornsson@outlook.com>
2025-10-27 16:25:39 +0100

    Duplicate2.c faktiskt färdig

--- C/Duplicate2.c
+++ C/Duplicate2.c
@@ -8,12 +8,13 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
     int i; 
     int x; 
     list[nums[0]] = 0;
-    for(int i = 1; i < numsSize; ++i){
+    for(int i = 1; i < numsSize; i++){
         x = nums[i]; 
-        if(0 == list[x]){
+        if(0 == list[x] && x != nums[0]){
             list[x] = i;
         }
         else{
+            //list[x] = i;
             if(i-list[x] <= k){
                 //printf("%d", i );
                 //printf("\n");
@@ -21,16 +22,23 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
                 //printf("\n");
                 return true;
             }
-        }
+        } 
+        //printf("%d", x );
+        //printf("\n");
+        //printf("%d", i );
+        //printf("\n");
+        //printf("%d", list[x]);
+        //printf("\n");
+        //printf("\n");
     }
     return false;
 } 
 
 int main () { 
-    int nums[] = {1,2,3,1,2,3};
+    int nums[] = {1,2,3,1};
     int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int k = 2;
-    printf("%d", containsNearbyDuplicate(nums, numsSize, k));
+    int k = 3;
+    printf("Result: %d", containsNearbyDuplicate(nums, numsSize, k));
     printf("\n");
     return 0;
 }
